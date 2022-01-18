2b3ce09cbb0eeb2574021bb1fb94492663a6ca7b
rannie ollit <rannie.ollit@fllexisourceit.com.au>
2022-01-18 08:15:35 +0800

    initial commit

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
+use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class QuestionController extends AbstractController
+{
+    private $logger;
+    private $isDebug;
+
+    public function __construct(LoggerInterface $logger, bool $isDebug)
+    {
+        $this->logger = $logger;
+        $this->isDebug = $isDebug;
+    }
+
+
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function homepage(QuestionRepository $questionRepository)
+    {
+        $questions = $questionRepository->findAllAskedOrderedByNewest();
+        return $this->render('question/homepage.html.twig', [
+            'questions' => $questions
+        ]);
+    }
+
+    /**
+     * @Route("/questions/new", methods={"GET"})
+     * @return Response
+     */
+    public function new(EntityManagerInterface $manager): Response
+    {
+        $question = new Question();
+        $question->setName('Missing pants')
+            ->setSlug('missing-pants-'.rand(0, 1000))
+            ->setQuestion(<<<EOF
+                Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+                to make my dishes wash themselves. But while I was casting it,
+                I slipped a little and I think `I also hit my pants with the spell`.
+                When I woke up this morning, I caught a quick glimpse of my pants
+                opening the front door and walking out! I've been out all afternoon
+                (with no pants mind you) searching for them.
+                Does anyone have a spell to call your pants back?
+                EOF
+            );
+
+        if(rand(1, 10) > 2) {
+            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+
+        $manager->persist($question);
+        $manager->flush();
+        return new Response(sprintf(
+            'Well hallo! The shiny new question is id #%d, slug: %s',
+            $question->getId(),
+            $question->getSlug()
+        ));
+    }
+
+    /**
+     * @Route("/questions/{slug}", name="app_question_show")
+     * @return Response
+     */
+    public function show(Question $question, MarkdownHelper $markdownHelper, QuestionRepository $repository)
+    {
+        if ($this->isDebug) {
+            $this->logger->info('We are in debug mode!');
+        }
+
+        $answers = [
+            'Make sure your cat is sitting `purrrfectly` still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => $question,
+            'answers' => $answers,
+        ]);
+    }
+
+}
